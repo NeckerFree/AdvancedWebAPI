c2d46a8d3827594ed5ee83a157ba7f7ba6cbc22c
Elio Cortes <elio.cortes3000@gmail.com>
2023-01-16 21:16:07 -0500

    Update Readme file

--- AWA.Repository/DependencyInjection.cs
+++ AWA.Repository/DependencyInjection.cs
@@ -1,13 +1,7 @@
 ﻿using AWA.DataAccess;
 using AWA.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AWA.Repository
 {
